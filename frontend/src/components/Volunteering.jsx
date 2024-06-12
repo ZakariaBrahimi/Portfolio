07d77d8a48221601c35c363fccde18f24962ac99
unknown <brahimi.zakaria.abdessamed@gmail.com>
2024-06-12 21:43:29 +0100

    Add Voluntering work

--- frontend/src/components/Volunteering.jsx
+++ frontend/src/components/Volunteering.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
 
 const Volunteering = () => {
   return (
-    <div className='animate-fade-up text-[#ffffffeb]'>Volunteering</div>
-  )
-}
+    <div className=" w-full h-80 font-black text-4xl flex gap-5 underline cursor-pointer justify-center items-center animate-fade-up text-[#ffffffeb] ">
+      <p>Coming Soon</p>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="w-10 h-10"
+      >
+        <polygon points="13 19 22 12 13 5 13 19" />
+        <polygon points="2 19 11 12 2 5 2 19" />
+      </svg>
+    </div>
+  );
+};
 
-export default Volunteering
+export default Volunteering;
